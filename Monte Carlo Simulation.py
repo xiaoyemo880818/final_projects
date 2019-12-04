4ca2bf7a7984bf47026c5f2c6c63f1e8d38d9dce
sophiafan <sophia.cation@gmail.com>
2019-12-03 22:03:49 -0600

    proposal

---
+++ Monte Carlo Simulation.py
@@ -0,0 +1,31 @@
+# use Numpy to do the simulation
+# calculate weekly profit= revenue-cost
+import numpy as np
+
+
+# calculate cost
+#---- single room cost---- from real-world data--https://www.kaggle.com/tianhwu/brooklynhomes2003to2017
+# *---- shuttle cost--- from real-world data  cost---- https://www.kaggle.com/dansbecker/new-york-city-taxi-fare-prediction
+# influence the guest number---https://www.kaggle.com/new-york-state/nys-air-passenger-traffic,-port-authority-of-ny-nj
+
+
+
+
+# calculate revenue
+#---- guest number--- it will be influence by location--from real-world data
+#                     https://www.kaggle.com/muonneutrino/new-york-city-census-data
+#                     https://www.kaggle.com/new-york-city/new-york-city-population
+
+# *---- room number---- random number[uniform distribution]
+
+
+
+# *---- room price level---- from real-world data find distribution
+#                         https://www.kaggle.com/airbnb/seattle
+
+
+
+
+# for-loop to do simulation and output the result
+
+# visualization the result
