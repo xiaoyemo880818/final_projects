72463e9b3407fd404ebd71d2e199d51660159b9f
linyaoli <linyaol2@illinois.edu>
2019-12-06 16:01:50 -0600

    new1

--- Monte Carlo Simulation.py
+++ Monte Carlo Simulation.py
@@ -4,29 +4,11 @@ import numpy as np
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-TotalGuestNum= 10000000
-# # total number of guest-----https://www.kaggle.com/new-york-state/nys-air-passenger-traffic,-port-authority-of-ny-nj
-#
-#
-#
-# # calculate cost
-# # Manhattan hotel cost http://www.cushmanwakefield.us/en/research-and-insight/2017/focus-on-hotel-construction-costs-2017
-# DowntownCost=950000
-# distance=np.random.   #(0,30] miles---- reference from googlemap---
-# singleRoomCost=DowntownCost/distance # reference from newyork house price----https://www1.nyc.gov/site/finance/taxes/property-rolling-sales-data.page
-#
-#
-# # * ---- shuttle cost--- from real-world data  cost---- https://www.kaggle.com/dansbecker/new-york-city-taxi-fare-prediction
-# # shuttle is binomial
-# shuttleCost=790  #every day bus cost reference----http://www.freightmetrics.com.au/Calculators%7CRoad/BusOperatingCost/tabid/671/Default.aspx
-# plusGuest=100*GuestType
-
-
 
 # calculate cost
 # room cost
 DowntownCost=950000 # Manhattan hotel cost http://www.cushmanwakefield.us/en/research-and-insight/2017/focus-on-hotel-construction-costs-2017
-
+distance=np.random.uniform(0, 30, 1) #(0,30] miles---- reference from googlemap---
 singleRoomCost=DowntownCost/distance # reference from new york house price----https://www1.nyc.gov/site/finance/taxes/property-rolling-sales-data.page
 
 #  * ---- shuttle cost--- from real-world data  cost---- https://www.kaggle.com/dansbecker/new-york-city-taxi-fare-prediction
@@ -34,17 +16,13 @@ singleRoomCost=DowntownCost/distance # reference from new york house price----ht
 shuttleCost=790  #every day bus cost reference----http://www.freightmetrics.com.au/Calculators%7CRoad/BusOperatingCost/tabid/671/Default.aspx
 plusGuest=100*GuestType
 
-
-
 # *location
 # suppose the guest number have linear negative relationship with distance
-distance=np.random.uniform(0, 30, 1) #(0,30] miles---- reference from googlemap---
 
 # Guest number of a certain hotel,nsuppose the guest number have linear negative relationship with distance
 def getGuestNum()->int:
     GuestInterval=(7000000,11000000)#[7478511,11444185]  it is a uniform distribution-----https://www.kaggle.com/new-york-state/nys-air-passenger-traffic,-port-authority-of-ny-nj
     TotalGuestNum=np.random.random_integers(GuestInterval[0],GuestInterval[1],1)
-
     HighestGuestRate=0.00018   # !!!!!lack of reference!
     LowestGuestRate=HighestGuestRate/2.5  #reference from TripAdvisor number of reviews
     k=(HighestGuestRate-LowestGuestRate)/30.0
@@ -53,24 +31,7 @@ def getGuestNum()->int:
     return hotelGuestNum
 
 
-
 # calculate revenue
-
-# *---- room number---- random number[uniform distribution]
-# get the distribution of people's travel type-- https://www.kaggle.com/enikolov/reviews-tripadvisor-hotels-and-edmunds-cars/data
-# analyze GuestType by https://www.kaggle.com/crawford/las-vegas-tripadvisor-reviews
-roomNum=np.random.random_integers(50, 2000, 1)  # reference Ctrip website
-GuestType=(0.19, 0.59, 0.22) #19% solo & business,59% couples & friends,22% families
-HotelGuestNum=TotalGuestNum*locationInfluence
-realBookedRoom=int(GuestType[0]*HotelGuestNum+GuestType[1]*HotelGuestNum/2+GuestType[2]*HotelGuestNum/3)
-if realBookedRoom < roomNum:
-    revenue=realBookedRoom*price
-# else revenue=roomNum*price
-
-
-
-
-
 
 # *---- room price level----
 def getPrice()-> int:
@@ -92,11 +53,18 @@ def getPrice()-> int:
 
 
 def calRevenue(totalPrice:int,hotelGuestNum:int)->int:
-    roomNum = np.random.[50, 2000]  # reference ctrip website
-    GuestType = []
+    # *---- room number---- random number[uniform distribution]
+    # get the distribution of people's travel type-- https://www.kaggle.com/enikolov/reviews-tripadvisor-hotels-and-edmunds-cars/data
+    # analyze GuestType by https://www.kaggle.com/crawford/las-vegas-tripadvisor-reviews
+    roomNum = np.random.random_integers(50, 2000, 1)  # reference Ctrip website
+    GuestType = (0.19, 0.59, 0.22)  # 19% solo & business,59% couples & friends,22% families
+    realBookedRoom = int(GuestType[0] * hotelGuestNum + GuestType[1] * hotelGuestNum / 2 + GuestType[2] * hotelGuestNum / 3)
+    if realBookedRoom < roomNum:
+        revenue = realBookedRoom * totalPrice
+    else:
+        revenue=roomNum*totalPrice
+    return revenue
 
-    realBookedRoom = HotelGuestNum * GuestType
-    return realBookedRoom < roomNum?realBookedRoom * totalPrice:roomNum * totalPrice
 
 def calCost():
     pass
